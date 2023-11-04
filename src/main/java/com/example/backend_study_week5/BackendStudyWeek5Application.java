b7ed087549c87c6da10c272c217030b6d2470d1a
kwonssshyeon <kwonsh4396@gmail.com>
2023-11-04 22:11:01 +0900

    initial commit

---
+++ src/main/java/com/example/backend_study_week5/BackendStudyWeek5Application.java
@@ -0,0 +1,13 @@
+package com.example.backend_study_week5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendStudyWeek5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendStudyWeek5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backend_study_week5/BackendStudyWeek5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_study_week5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendStudyWeek5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
